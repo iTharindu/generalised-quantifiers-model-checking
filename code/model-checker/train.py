254c14753943e69d82d2cba1a6979facc0ff2c19
iTharindu <stharindu.16@cse.mrt.ac.lk>
2024-01-25 16:16:40 +0000

    train file setup

---
+++ code/model-checker/train.py
@@ -0,0 +1,206 @@
+import transformers
+from datasets import load_dataset, load_metric
+import numpy as np
+import pandas as pd
+
+import argparse
+import json
+import logging
+import math
+import os
+import random
+from pathlib import Path
+
+import datasets
+import evaluate
+import torch
+from accelerate import Accelerator
+from accelerate.logging import get_logger
+from accelerate.utils import set_seed
+from datasets import load_dataset
+from huggingface_hub import Repository, create_repo
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+import transformers
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    PretrainedConfig,
+    SchedulerType,
+    default_data_collator,
+    get_scheduler,
+    T5Config,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+    T5Model,
+    T5ForSequenceClassification,
+)
+
+from transformers.utils import check_min_version, get_full_repo_name, send_example_telemetry
+from transformers.utils.versions import require_version
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Finetune a transformers model on a text classification task")
+
+    parser.add_argument(
+        "--with_tracking",
+        action="store_true",
+        help="Whether to enable experiment trackers for logging.",
+    )
+
+    parser.add_argument(
+        "--ignore_mismatched_sizes",
+        action="store_true",
+        help="Whether or not to enable to load a pretrained model whose head dimensions are different.",
+    )
+
+    
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--pad_to_max_length",
+        default=True,
+        help="If passed, pad all samples to `max_length`. Otherwise, dynamic padding is used.",
+    )
+
+    parser.add_argument(
+        "--task_name",
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument(
+        "--mixed_precision",
+        default="fp16",
+        type=str,
+        help="Setting up the mixed precision Eg: fp16",
+    )
+
+    parser.add_argument("--output_dir", type=str, default=None, help="Where to store the final model.")
+
+    parser.add_argument("--seed", type=int, default=None, help="A seed for reproducible training.")
+
+    parser.add_argument(
+        "--checkpointing_steps",
+        type=str,
+        default=None,
+        help="Whether the various states should be saved at the end of every n steps, or 'epoch' for each epoch.",
+    )
+    parser.add_argument(
+        "--max_train_steps",
+        type=int,
+        default=None,
+        help="Total number of training steps to perform. If provided, overrides num_train_epochs.",
+    )
+    parser.add_argument(
+        "--resume_from_checkpoint",
+        type=str,
+        default=None,
+        help="If the training should continue from a checkpoint folder.",
+    )
+
+    parser.add_argument(
+        "--metric",
+        type=str,
+        default="accuracy",
+        help="Setting up the metric, eg: accuracy, precision",
+    )
+
+
+
+    parser.add_argument(
+        "--train_file", type=str, default=None, help="A csv or a json file containing the training data."
+    )
+    parser.add_argument(
+        "--validation_file", type=str, default=None, help="A csv or a json file containing the validation data."
+    )
+
+    parser.add_argument(
+        "--out_file", type=str, default=None, help="A csv or a json file containing the validation data."
+    )
+
+    parser.add_argument(
+        "--use_slow_tokenizer",
+        default=False,
+        action="store_true",
+        help="If passed, will use a slow tokenizer (not backed by the Tokenizers library).",
+    )
+    
+
+    parser.add_argument(
+        "--max_length",
+        type=int,
+        default=768,
+        help=(
+            "The maximum total input sequence length after tokenization. Sequences longer than this will be truncated,"
+            " sequences shorter will be padded if `--pad_to_max_length` is passed."
+        ),
+    )
+
+    parser.add_argument(
+        "--per_device_train_batch_size",
+        type=int,
+        default=64,
+        help="Batch size (per device) for the training dataloader.",
+    )
+    parser.add_argument(
+        "--per_device_eval_batch_size",
+        type=int,
+        default=64,
+        help="Batch size (per device) for the evaluation dataloader.",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=5e-5,
+        help="Initial learning rate (after the potential warmup period) to use.",
+    )
+
+    parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay to use.")
+    parser.add_argument("--num_train_epochs", type=int, default=3, help="Total number of training epochs to perform.")
+
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=4,
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
+    )
+    parser.add_argument(
+        "--gradient_checkpointing",
+        default=True,
+        help="Whether or not to enable gradient checkpointing",
+    )
+
+    parser.add_argument(
+        "--num_labels",
+        default=2,
+        type=int,
+        help="Number of labels for the classifier.",
+    )
+
+    parser.add_argument(
+        "--lr_scheduler_type",
+        type=SchedulerType,
+        default="linear",
+        help="The scheduler type to use.",
+        choices=["linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"],
+    )
+    parser.add_argument(
+        "--num_warmup_steps", type=int, default=0, help="Number of steps for the warmup in the lr scheduler."
+    )
+
+    args = parser.parse_args()
+
+    return args
+
